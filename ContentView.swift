f9bbefd24a12d509e0f6ec1a9b0032fcf7ad624e
shuntaro <taro.may11@gmail.com>
2023-07-24 03:28:40 +0900

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var okashiDataList = OkashiData()
+    
+    @State var inputText = ""
+    
+    @State var showSafari = false
+    
+    var body: some View {
+        VStack {
+            
+            TextField("キーワード", text: $inputText
+                      , prompt: Text("キーワードを入力してください"))
+            .onSubmit {
+                
+                Task {
+                    await okashiDataList.searchOkashi(keyword: inputText)
+                }
+            }
+            
+            .submitLabel(.search)
+            
+            .padding()
+            
+            List(okashiDataList.okashiList) { okashi in
+                
+                Button(action: {
+                    
+                    
+                    showSafari.toggle()
+                    
+                }) {
+                    
+                    HStack {
+                        
+                        AsyncImage(url: okashi.image) { image in
+                            
+                            image
+                                .resizable()
+                            
+                                .aspectRatio(contentMode: .fit)
+                            
+                                .frame(height: 40)
+                        } placeholder: {
+                            
+                            ProgressView()
+                            
+                        }
+                        Text(okashi.name)
+                    }
+                    
+                }
+                .sheet(isPresented: self.$showSafari, content: {
+                    
+                    SafariView(url: okashi.link)
+                    
+                        .edgesIgnoringSafeArea(.bottom)
+                })
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myOkashiApp.swift
+//  myOkashi
+//
+//  Created by 菅家俊太郎 on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct myOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
